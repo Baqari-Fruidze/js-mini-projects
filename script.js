b37697497fe77cb0213cd9b1ea47c3d625369a8b
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-02-15 14:51:05 +0400

    second

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 //   console.log(`hello ${name} ,Nice to meet you!`);
 // }
 // sayHello();
+
+// პროექტი 2 - Counting the Number of Characters - სიმბოლოების რაოდენობის დათვლა
+// let answer = prompt("please enter your sentence ");
+// console.log(`your sentence consists from  ${answer.length} characters`);
